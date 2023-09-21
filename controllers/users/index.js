239c9b96d9f65a188ef6ae27e609f41c5da40da0
Dima Savchuk <dima.savchuk.011@gmail.com>
2023-09-21 13:19:57 +0300

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,21 +1,9 @@
 const getCurrent = require("./getCurrent");
-<<<<<<< HEAD
-const logout = require("./logout");
-const updateAvatar = require("./updateAvatar");
-const updateSubscription = require("./updateSubscription");
-=======
 const updateUser = require("./updateUser");
 const subscribeEmail = require("./subscribeEmail");
->>>>>>> MolchanovSergii-users_auth
 
 module.exports = {
   getCurrent,
-<<<<<<< HEAD
-  logout,
-  updateAvatar,
-  updateSubscription,
-=======
   updateUser,
   subscribeEmail,
->>>>>>> MolchanovSergii-users_auth
 };
